7d8b2214fbd4544581a65d16e15d570aec8ca8b7
Daniel Scherzer <daniel.e.scherzer@gmail.com>
2025-06-14 22:08:08 -0700

    Revert attributes/name/etc.
    Just switching to a zend_class_alias object should be easier

--- ext/reflection/php_reflection_arginfo.h
+++ ext/reflection/php_reflection_arginfo.h
@@ -1,5 +1,5 @@
 /* This is a generated file, edit php_reflection.stub.php instead.
- * Stub hash: 360dcefc11527b30c94cc59f8475a25a8a0a90ca
+ * Stub hash: 0c4bbfe6c6f50fbd6dcc994a25fb248bd2928ef3
  * Has decl header: yes */
 
 ZEND_BEGIN_ARG_WITH_TENTATIVE_RETURN_TYPE_INFO_EX(arginfo_class_Reflection_getModifierNames, 0, 1, IS_ARRAY, 0)
@@ -737,10 +737,6 @@ ZEND_END_ARG_INFO()
 
 #define arginfo_class_ReflectionClassAlias___toString arginfo_class_ReflectionFunction___toString
 
-#define arginfo_class_ReflectionClassAlias_isDeprecated arginfo_class_ReflectionFunctionAbstract_hasTentativeReturnType
-
-#define arginfo_class_ReflectionClassAlias_getAttributes arginfo_class_ReflectionFunctionAbstract_getAttributes
-
 ZEND_METHOD(Reflection, getModifierNames);
 ZEND_METHOD(ReflectionClass, __clone);
 ZEND_METHOD(ReflectionFunctionAbstract, inNamespace);
@@ -1016,8 +1012,6 @@ ZEND_METHOD(ReflectionConstant, __toString);
 ZEND_METHOD(ReflectionConstant, getAttributes);
 ZEND_METHOD(ReflectionClassAlias, __construct);
 ZEND_METHOD(ReflectionClassAlias, __toString);
-ZEND_METHOD(ReflectionClassAlias, isDeprecated);
-ZEND_METHOD(ReflectionClassAlias, getAttributes);
 
 static const zend_function_entry class_Reflection_methods[] = {
 	ZEND_ME(Reflection, getModifierNames, arginfo_class_Reflection_getModifierNames, ZEND_ACC_PUBLIC|ZEND_ACC_STATIC)
@@ -1397,8 +1391,6 @@ static const zend_function_entry class_ReflectionConstant_methods[] = {
 static const zend_function_entry class_ReflectionClassAlias_methods[] = {
 	ZEND_ME(ReflectionClassAlias, __construct, arginfo_class_ReflectionClassAlias___construct, ZEND_ACC_PUBLIC)
 	ZEND_ME(ReflectionClassAlias, __toString, arginfo_class_ReflectionClassAlias___toString, ZEND_ACC_PUBLIC)
-	ZEND_ME(ReflectionClassAlias, isDeprecated, arginfo_class_ReflectionClassAlias_isDeprecated, ZEND_ACC_PUBLIC)
-	ZEND_ME(ReflectionClassAlias, getAttributes, arginfo_class_ReflectionClassAlias_getAttributes, ZEND_ACC_PUBLIC)
 	ZEND_FE_END
 };
 
--- ext/reflection/php_reflection_decl.h
+++ ext/reflection/php_reflection_decl.h
@@ -1,12 +1,12 @@
 /* This is a generated file, edit php_reflection.stub.php instead.
- * Stub hash: 360dcefc11527b30c94cc59f8475a25a8a0a90ca */
+ * Stub hash: 0c4bbfe6c6f50fbd6dcc994a25fb248bd2928ef3 */
 
-#ifndef ZEND_PHP_REFLECTION_DECL_360dcefc11527b30c94cc59f8475a25a8a0a90ca_H
-#define ZEND_PHP_REFLECTION_DECL_360dcefc11527b30c94cc59f8475a25a8a0a90ca_H
+#ifndef ZEND_PHP_REFLECTION_DECL_0c4bbfe6c6f50fbd6dcc994a25fb248bd2928ef3_H
+#define ZEND_PHP_REFLECTION_DECL_0c4bbfe6c6f50fbd6dcc994a25fb248bd2928ef3_H
 
 typedef enum zend_enum_PropertyHookType {
 	ZEND_ENUM_PropertyHookType_Get = 1,
 	ZEND_ENUM_PropertyHookType_Set = 2,
 } zend_enum_PropertyHookType;
 
-#endif /* ZEND_PHP_REFLECTION_DECL_360dcefc11527b30c94cc59f8475a25a8a0a90ca_H */
+#endif /* ZEND_PHP_REFLECTION_DECL_0c4bbfe6c6f50fbd6dcc994a25fb248bd2928ef3_H */
